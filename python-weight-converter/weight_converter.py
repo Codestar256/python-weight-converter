517072a6501b5348d29b5dd2c2ee6c701a9251c6
Codestar256 <gloriamirembe020@gmail.com>
2026-06-04 14:41:52 +0300

    Fix case sensitivity in unit input handling
    Ensure unit input is case-insensitive for weight conversion.

--- python-weight-converter/weight_converter.py
+++ python-weight-converter/weight_converter.py
@@ -2,9 +2,9 @@ user_weight = input("Enter your weight:")
 unit = input("l(bs) or K(g):")
 
 
-if "l" == unit:
+if "l" == unit.lower():
     converted_weight = int(user_weight) * 0.45
     print(f"{converted_weight} k(g)")
-elif "k" == unit:
+elif "k" == unit.lower:
     converted_weight = int(user_weight) / 0.45
     print(f"{converted_weight} l(bs)")
